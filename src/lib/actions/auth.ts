3648dd495de0270daa30ab98577bf92222c0d4e5
NLut <Natapat2001@gmail.com>
2025-11-10 04:20:20 +0700

    Feat: auth actions

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -3,9 +3,9 @@
 import { signIn, signOut } from "~/server/auth";
 
 export const login = async () => {
-    await signIn("github", { redirectTo: "/" }); // wait github correctly finished sign in then redirect to home page
+    await signIn("github", { redirectTo: "/home" }); // wait github correctly finished sign in then redirect to home page
 };
 
 export const logout = async () => {
-    await signOut({ redirectTo: "/" });
+    await signOut({ redirectTo: "/login-page" });
 };
